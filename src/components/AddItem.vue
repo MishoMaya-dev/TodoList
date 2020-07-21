a500824e1baf69113e53e70ced607936b780d410
Misho Maya <792799580mm@gmail.com>
2020-07-21 20:55:55 +0300

    Привет

---
+++ src/components/AddItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <input type="text" v-model="title">
+    <button @click="createItem">Create</button>
+  </div>
+</template>
+
+<script>
+	export default {
+		name: "AddItem",
+    data: () => ({
+      title: ''
+    }),
+    methods: {
+			createItem() {
+				this.$emit('create-item', this.title);
+				this.title = '';
+      }
+    }
+	}
+</script>
+
+<style scoped>
+  div {
+    display: flex;
+    justify-content: space-between;
+    padding: 0 10px;
+    margin-bottom: 20px;
+  }
+  input {
+    width: 70%;
+    height: 27px;
+  }
+  button {
+    width: 25%;
+    height: 27px;
+  }
+</style>
